3046372cafe0e8f637e61b329447897ef9caca32
Pedro <pedro1silva1vargas@gmail.com>
2023-01-02 15:04:05 -0300

    Initial Commit

---
+++ ForzaDash/ForzaDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForzaDashApp.swift
+//  ForzaDash
+//
+//  Created by Pedro Vargas on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ForzaDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
