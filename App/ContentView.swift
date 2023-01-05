49342ca903e10dc66bd1d708b2058456fb568b4f
Pedro <pedro1silva1vargas@gmail.com>
2023-01-05 01:44:08 -0300

    Update gear and other values and implement UI V1

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -34,41 +34,163 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Group {
-                Color.blue
-                Color.black.opacity(0.7)
-            }
-            .edgesIgnoringSafeArea(.all)
+            Color.black
+                .edgesIgnoringSafeArea(.all)
             
-            Group {
-                VStack {
-                    HStack {
-                        Image(systemName:"flag.checkered.circle")
-                            .symbolRenderingMode(.palette)
-                            .if(
-                                viewModel.data?.gameIsRunning ?? false,
-                                transform: {
-                                    $0.foregroundStyle(.white, .green)
-                                },
-                                elseTransform: { $0.foregroundColor(.red)
-                                }
-                            )
-                                .font(.init(.system(size: 35)))
-                                .imageScale(.large)
-                                Spacer()
+            VStack(alignment: .center, spacing: 16) {
+                rpmProgressView
+                    .padding(.top, 6)
+                HStack(spacing: 16) {
+                    VStack(spacing: 16) {
+                        rpmView
+                        speedView
                     }
                     Spacer()
-                }
-
-                Text("\(viewModel.data?.speed ??  0) KM/h")
-                    .foregroundColor(.white)
-                    .font(.init(.system(size: 50)))
-                    .onAppear {
-                        viewModel.load()
+                    gearView
+                    Spacer()
+                    VStack(spacing: 16) {
+                        isRaceOnView
+                        boostView
                     }
+//                    boostView
+                }
             }
         }
+        .onAppear {
+            viewModel.load()
+        }
         
+        
+    }
+    
+    var rpmProgressView: some View {
+        ProgressView(
+            value: viewModel.data?.currentEngineRPM ?? 0,
+            total: viewModel.data?.maxRPM ?? 0
+        )
+        .progressViewStyle(RPMProgressViewStyle())
+    }
+
+    var isRaceOnView: some View {
+        Rectangle()
+            .stroke(lineWidth: 3)
+            .frame(width: 200, height: 100)
+            .foregroundColor(.white)
+            .overlay(
+                Image(systemName:"flag.checkered.circle")
+                    .symbolRenderingMode(.palette)
+                    .if(
+                        viewModel.data?.gameIsRunning ?? false,
+                        transform: {
+                            $0.foregroundStyle(.white, .green)
+                        },
+                        elseTransform: { $0.foregroundColor(.red)
+                        }
+                    )
+                        .font(.init(.system(size: 35)))
+                        .imageScale(.large)
+            )
+            .overlay(alignment: .top) {
+                Text("RACING ON")
+                    .padding(.horizontal, 8)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .offset(y: -10)
+            }
+        
+    }
+    
+    var gearView: some View {
+        Rectangle()
+            .stroke(lineWidth: 3)
+            .frame(width: 150, height: 260)
+            .foregroundColor(.white)
+            .overlay(
+                Text(String(viewModel.data?.gear ?? 0))
+                    .foregroundColor(.white)
+                    .font(.init(.system(size: 85)))
+            )
+            .overlay(alignment: .top) {
+                Text("G E A R")
+                    .padding(.horizontal, 8)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .offset(y: -10)
+            }
+    }
+    
+    var rpmView: some View {
+        Rectangle()
+            .stroke(lineWidth: 3)
+            .frame(width: 200, height: 100)
+            .foregroundColor(.white)
+            .overlay(
+                Text(String(Int(viewModel.data?.currentEngineRPM ?? 00)))
+                    .foregroundColor(.white)
+                    .font(.init(.system(size: 40)))
+            )
+            .overlay(alignment: .top) {
+                Text("R P M")
+                    .padding(.horizontal, 8)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .offset(y: -10)
+            }
+    }
+    
+    var speedView: some View {
+        Rectangle()
+            .stroke(lineWidth: 3)
+            .if(
+                viewModel.data?.speed ?? 0 > 0,
+                transform: {
+                    $0.background(
+                        Color.green.opacity(0.7)
+                            .frame(width: 200, height: 150)
+                    )
+                },
+                elseTransform: {
+                    $0.background(
+                        Color.black
+                            .frame(width: 200, height: 150)
+                    )
+                }
+            )
+            .frame(width: 200, height: 150)
+            
+            .foregroundColor(.white)
+            .overlay(
+                Text(String(viewModel.data?.speed ??  0))
+                .foregroundColor(.white)
+                .font(.init(.system(size: 60)))
+            )
+            .overlay(alignment: .top) {
+                Text("K M / H")
+                    .padding(.horizontal, 8)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .offset(y: -10)
+            }
+        
+    }
+    
+    var boostView: some View {
+        Rectangle()
+            .stroke(lineWidth: 3)
+            .frame(width: 200, height: 150)
+            .foregroundColor(.white)
+            .overlay(
+                Text(String(viewModel.data?.boost ??  0))
+                .foregroundColor(.white)
+                .font(.init(.system(size: 60)))
+            )
+            .overlay(alignment: .top) {
+                Text("P S I")
+                    .padding(.horizontal, 8)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .offset(y: -10)
+            }
         
     }
 }
@@ -92,6 +214,22 @@ extension View {
             transform(self)
         } else {
             elseTransform(self)
+        }
+    }
+}
+
+struct RPMProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        return GeometryReader { gproxy in
+            Rectangle()
+                .stroke(lineWidth: 3)
+                .frame(height: 60)
+                .foregroundColor(.white)
+                .background(alignment: .leading) { Color.red
+                        .frame(width: CGFloat(configuration.fractionCompleted ?? 0) * gproxy.size.width
+                        )
+                }
+            //TODO: break into 3 parts green yellow and red
         }
     }
 }
