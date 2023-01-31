8abbca6d96781bd6825a7e7404562ca0985d8b75
Pedro <pedro1silva1vargas@gmail.com>
2023-01-31 14:10:18 -0300

    Create extensions and ui components

---
+++ Modules/Sources/DashFeature/UIComponentes-Module/SimpleProgressViewStyle.swift
@@ -0,0 +1,28 @@
+import Foundation
+import SwiftUI
+
+struct SimpleRPMProgressViewStyle: ProgressViewStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        return GeometryReader { gProxy in
+            ZStack {
+                Rectangle()
+                    .stroke(lineWidth: 3)
+                    .frame(height: 60)
+                    .foregroundColor(.white)
+                    .background(alignment: .leading) {
+                        Color.red
+                            .frame(
+                                width: CGFloat(configuration.fractionCompleted ?? 0) * gProxy.size.width
+                            )
+                    }
+            }
+        }
+    }
+}
+
+
+extension View {
+    func simpleRPMProgressStyle() -> some View {
+        self.progressViewStyle(SimpleRPMProgressViewStyle())
+    }
+}
