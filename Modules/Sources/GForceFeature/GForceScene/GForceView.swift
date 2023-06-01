8c2e60db2a46b7ee726d555e1981507daba8db89
Pedro <pedro1silva1vargas@gmail.com>
2023-06-01 13:02:25 -0300

    Update gforce view

--- Modules/Sources/GForceFeature/GForceScene/GForceView.swift
+++ Modules/Sources/GForceFeature/GForceScene/GForceView.swift
@@ -17,44 +17,38 @@ public struct GForceView: View {
             ZStack {
                 Color.black
                     .edgesIgnoringSafeArea(.all)
-                if
-                    let accelerationX = viewStore.state.model.accelerationX,
-                    let accelerationY = viewStore.state.model.accelerationY {
-                    Rectangle()
-                        .stroke(lineWidth: 3)
-                        .foregroundColor(.red)
-                        .overlay {
-                            ZStack {
-                                Path() { path in
-                                    path.move(to: .init(x: 100 * 2, y: 100 * 2))
-                                    path.addLine(to: .init(x: 100 * 2 + accelerationX / 27 * 2, y: 100 * 2 + accelerationY / 27 * 2))
-                                }
-                                .stroke(lineWidth: 1)
-                                .fill(Color.red)
-                                
-                                Circle()
-//                                    .clipped()
-                                    .foregroundColor(Color.yellow)
-                                    .frame(width: 10, height:10)
-                                    .position(x: 100 * 2 + accelerationX / 27 * 2, y: 100 * 2 + accelerationY / 27 * 2)
+                Rectangle()
+                    .stroke(lineWidth: 3)
+                    .foregroundColor(.red)
+                    .overlay {
+                        ZStack {
+                            Path() { path in
+                                path.move(to: .init(x: 100 * 2, y: 100 * 2))
+                                path.addLine(to: .init(x: 100 * 2 + viewStore.state.model.accelerationX / 27 * 2, y: 100 * 2 + viewStore.state.model.accelerationY / 27 * 2))
                             }
-                            .clipped()
+                            .stroke(lineWidth: 1)
+                            .fill(Color.red)
                             
+                            Circle()
+                                .foregroundColor(Color.yellow)
+                                .frame(width: 10, height:10)
+                                .position(x: 100 * 2 + viewStore.state.model.accelerationX / 27 * 2, y: 100 * 2 + viewStore.state.model.accelerationY / 27 * 2)
                         }
-                        .frame(width: 200 * 2, height: 200 * 2, alignment: .center)
-                }
-                
+                        .clipped()
+                        
+                    }
+                    .frame(width: 200 * 2, height: 200 * 2, alignment: .center)
             }
             .if(
                 viewStore.state.model.gameIsRunning == false, transform: {
                     $0.overlay(alignment: .topLeading) {
                         Text(viewStore.state.ip)
-                                .foregroundColor(.white)
-                                .background(Color.black)
+                            .foregroundColor(.white)
+                            .background(Color.black)
                     }
                 }
             )
-            .onAppear {
+                .onAppear {
                 viewStore.send(.onAppear)
             }
         }
